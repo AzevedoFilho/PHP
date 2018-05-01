ba550a07dc454a636f52431a8df41b93f7c8d694
Azevedo Filho <azevedofilho.oracle@outlook.com>
2018-05-01 16:18:26 -0300

    Arquivos de exemplo 8

---
+++ MyExampleCount/contar.php
@@ -0,0 +1,11 @@
+<?php
+
+// Para contar caracteres
+$str = "abcde3f";
+echo strlen($str); // 6
+ 
+/* Para contar palavras
+$str2 = 'aqui são quatro palavras';
+echo str_word_count($str2); // 4 */
+
+?>
